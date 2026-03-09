876b7702f25aed2067cc56bcfda63a27f75f3230
Aung Phyo Htet <aungphyohtet1@gmail.com>
2026-03-10 04:11:32 +0700

    familarize with the charts on shadcn. I was really afraid about the recharts but now it seems easy.

--- shadcn/app/page.tsx
+++ shadcn/app/page.tsx
@@ -1,11 +1,12 @@
 import { AppAreaChart } from "@/components/AppAreaChart";
 import AppChart from "@/components/AppChart";
+import AppPieChart from "@/components/AppPieChart";
 
 export default function HomePage() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 p-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppChart/></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppAreaChart/></div>
