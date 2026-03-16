c522eeb4c9a67b86f87b63c73c3005eda8125528
Aung Phyo Htet <aungphyohtet1@gmail.com>
2026-03-16 17:55:12 +0700

    feat: 	product-service 	add clerk/express 	use .env file on dev script.

--- ecommerce/apps/product-service/src/index.ts
+++ ecommerce/apps/product-service/src/index.ts
@@ -1,20 +1,26 @@
-import express, { Request, Response } from "express"
-import cors from "cors"
-const app = express()
-const port = 8000
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 
-app.use(cors({
-	origin: ['http://localhost:3000']
-}))
+const app = express();
+const port = 8000;
 
-app.get('/health', (req: Request, res: Response) => {
-  res.json({
-	status: "ok",
-	uptime: process.uptime(),
-	timestamp: Date.now()
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
   })
-})
+);
+
+app.use(clerkMiddleware());
+
+app.get("/health", (req: Request, res: Response) => {
+  res.json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
